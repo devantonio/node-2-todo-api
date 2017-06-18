2d504daba981745b9c90ff0db34c16daebb8719c
devantonio25 <antonio.ae91@gmail.com>
2017-06-18 17:58:35 -0400

    added delete methods

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+//query the mongodb database
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	} 
+	console.log('Connected to MongoDB server');
+
+	//deleteMany
+	// db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	//deleteOne
+	// db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+	// 	console.log(result.result);
+	// });
+
+	//findOneAndDelete
+	db.collection('Todos').findOneAndDelete({text: 'eat lunch'}).then((result) => {
+		console.log(result);
+	});
+
+//db.close();
+
+});
