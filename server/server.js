418e8edabdfc74fe3392455ebe226ce544430e9b
devantonio25 <antonio.ae91@gmail.com>
2017-06-23 19:44:40 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const {ObjectID} = require('mongodb');
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -38,6 +40,33 @@ app.get('/users', (req, res) => {
 		res.status(400).send(e);
 	});
 });
+
+
+
+
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
+});
+
+
+
+
+
 
 
 app.listen(3000, () => {
