823cff0ac3449f609fdb630e036a8a670f6dea5c
devantonio25 <antonio.ae91@gmail.com>
2017-06-23 17:26:56 -0400

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.post('/todos', (req, res) => {
 	var todo = new Todo({
 		text: req.body.text
 	});
+
 	//console.log(req.body);
 	todo.save().then((doc) => {
 		res.send(doc);
@@ -21,6 +22,23 @@ app.post('/todos', (req, res) => {
 		res.status(400).send(e);
 	});
 });
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/users', (req, res) => {
+	Todo.find().then((users) => {
+		res.send({users});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
 
 app.listen(3000, () => {
 	console.log('server is up on port 3000');
